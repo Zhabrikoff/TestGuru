10bd608396fda8bf4d228af3775f137058012a5a
Zhabrikoff <ezhabrikov@gmail.com>
2024-05-09 20:41:28 +0300

    feat: add class method sorting tests by category for Test class

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.sorting_tests_by_category(category_name)
+    Test.joins('JOIN categories ON tests.category_id = categories.id')
+        .where(categories: { title: category_name })
+        .order(title: :desc)
+        .pluck(:title)
+  end
 end
