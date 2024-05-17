62dfef4e22546e1a90d48671c4390145fc5a2367
Zhabrikoff <ezhabrikov@gmail.com>
2024-05-17 19:45:10 +0300

    feat: add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  has_many :questions
+  has_many :tests_statuses
+  has_many :users, through: :tests_statuses
+
   def self.sorting_tests_by_category(category_name)
     Test.joins('JOIN categories ON tests.category_id = categories.id')
         .where(categories: { title: category_name })
