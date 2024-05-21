96adbf7514d0af0932b3fea54b32035fd57915f8
Zhabrikoff <ezhabrikov@gmail.com>
2024-05-21 18:38:45 +0300

    fix: fix tests_by_level in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
 
   def tests_by_level(level)
-    Test.joins('JOIN tests_statuses ON tests.id = tests_statuses.test_id').where(
-      'tests_statuses.user_id = ? AND tests.level = ?', id, level
-    )
+    tests.where(level: level)
   end
 end
