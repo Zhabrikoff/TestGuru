db3251c5e9656f9f3dc5450ed5d02253139ff376
Zhabrikoff <ezhabrikov@gmail.com>
2024-06-27 20:39:17 +0300

    refactor: refactor test_passage model

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,8 +3,9 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_validation :set_next_question, on: :update
+  before_validation :before_validation_set_current_question, on: %i[create update]
+
+  MINIMUM_SUCCESS_PERCENTAGE = 85
 
   def completed?
     current_question.nil?
@@ -15,18 +16,22 @@ class TestPassage < ApplicationRecord
     save!
   end
 
-  def set_next_question
-    self.current_question = next_question
-  end
-
   def test_successful?
-    (correct_questions.to_f / test.questions.count * 100).round(2) >= 85
+    (correct_questions.to_f / test.questions.count * 100).round(2) >= MINIMUM_SUCCESS_PERCENTAGE
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_current_question
+    if current_question.nil?
+      self.current_question = test.questions.first if test.present?
+    else
+      self.current_question = next_question
+    end
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
   def correct_answer?(answer_ids)
@@ -35,9 +40,5 @@ class TestPassage < ApplicationRecord
 
   def correct_answers
     current_question.answers.correct
-  end
-
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
