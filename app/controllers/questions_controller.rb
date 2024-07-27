3e475dbf47fd4ba0a514225301f03e23657139af
Zhabrikoff <ezhabrikov@gmail.com>
2024-07-27 20:20:37 +0300

    feat: add devise gem

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,42 +1,12 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index new create]
-  before_action :find_question, only: %i[show destroy edit update]
+  before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[show]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index; end
 
   def show; end
-
-  def new
-    @question = @test.questions.new
-  end
-
-  def create
-    @question = @test.questions.new(question_params)
-
-    if @question.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def destroy
-    @question.destroy
-
-    redirect_to test_path(@question.test)
-  end
-
-  def edit; end
-
-  def update
-    if @question.update(question_params)
-      redirect_to test_path(@question.test)
-    else
-      render :edit
-    end
-  end
 
   private
 
@@ -46,10 +16,6 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = Question.find(params[:id])
-  end
-
-  def question_params
-    params.require(:question).permit(:body)
   end
 
   def rescue_with_question_not_found
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,9 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show start]
+  before_action :find_test, only: %i[start]
 
   def index
     @tests = Test.all
   end
-
-  def show; end
 
   def start
     current_user.tests.push(@test)
@@ -17,9 +15,5 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 end
