5778e150712dd9c09d26a8ffbfd10f437822afeb
Zhabrikoff <ezhabrikov@gmail.com>
2024-07-27 20:20:37 +0300

    feat: add devise gem

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,33 @@
+class Admin::TestsController < Admin::BaseController
+  before_action :find_test, only: %i[show]
+
+  def index
+    @tests = Test.all
+  end
+
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = current_user.author_tests.new(test_params)
+
+    if @test.save
+      redirect_to [:admin, @test]
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,21 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
+
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
   has_many :test_passages
   has_many :tests, through: :test_passages
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
-  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def tests_by_level(level)
     tests.where(level: level)
