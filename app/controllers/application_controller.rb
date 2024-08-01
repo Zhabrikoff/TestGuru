dc78f7f47174f1a6a8d7610f7382bd3ac4eba7ea
Zhabrikoff <ezhabrikov@gmail.com>
2024-08-01 20:55:23 +0300

    feat: add internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
@@ -13,5 +14,13 @@ class ApplicationController < ActionController::Base
     else
       root_path
     end
+  end
+
+  def default_url_options
+    { lang: (I18n.locale == I18n.default_locale ? nil : I18n.locale) }
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
