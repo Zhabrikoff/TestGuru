831e0855fc662e5613237d1bf5f487bced212cc2
Zhabrikoff <ezhabrikov@gmail.com>
2024-08-08 20:31:25 +0300

    feat: add bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  TYPES_OF_ALERTS = {
+    notice: 'success',
+    alert: 'danger',
+    error: 'danger',
+    warning: 'warning',
+    info: 'primary'
+  }.freeze
+
   def current_year
     Time.current.year
   end
@@ -9,7 +17,7 @@ module ApplicationHelper
 
   def flash_messages
     flash.map do |type, message|
-      content_tag(:p, message, class: "flash #{type}")
+      content_tag(:div, message, class: "alert alert-#{TYPES_OF_ALERTS[type.to_sym]}")
     end.join.html_safe
   end
 end
