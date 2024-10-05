434c68cab95bb615a9cdfbc794f208845a339872
Zhabrikoff <ezhabrikov@gmail.com>
2024-10-05 14:45:28 +0300

    fix: some fixes

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -8,7 +8,7 @@ class TestPassagesController < ApplicationController
   def update
     @test_passage.accept!(params[:answer_ids])
 
-    if @test_passage.completed?
+    if @test_passage.completed? || @test_passage.time_is_up?
       redirect_to result_test_passage_path(@test_passage)
     else
       render :show
